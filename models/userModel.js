61dd0ee5b4b2a5c6bbb2eadb8724edde0b69bbea
akberofh <sd85bejal@code.edu.az>
2024-11-02 00:44:29 +0400

    guyguy

--- models/userModel.js
+++ models/userModel.js
@@ -16,6 +16,11 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    photo: {
+      type: String, 
+   
+      default: '',
+    },
   },
   { timestamps: true }
 );
